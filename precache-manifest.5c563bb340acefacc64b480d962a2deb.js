818f2502d989d74633fa4fa29ad971e889b8f876
Nguyễn Văn Huy <huynguyen1998169@gmail.com>
2020-07-03 18:49:08 +0700

    Updates

---
+++ precache-manifest.5c563bb340acefacc64b480d962a2deb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1612ae849e5b85060f21f83d9b689fd6",
+    "url": "/Website/index.html"
+  },
+  {
+    "revision": "4df4ee24b5030ac3cbf6",
+    "url": "/Website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ebf2ff8c29902ca55492",
+    "url": "/Website/static/css/main.e71e026b.chunk.css"
+  },
+  {
+    "revision": "4df4ee24b5030ac3cbf6",
+    "url": "/Website/static/js/2.b1ae4462.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Website/static/js/2.b1ae4462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf2ff8c29902ca55492",
+    "url": "/Website/static/js/main.8db97116.chunk.js"
+  },
+  {
+    "revision": "cdda4c94e88107f07968",
+    "url": "/Website/static/js/runtime-main.3b753446.js"
+  },
+  {
+    "revision": "acd88f29080f817f6b948161bac3f7ef",
+    "url": "/Website/static/media/logo.acd88f29.png"
+  }
+]);
